7ba43ccdd504ec7d9d7acf72524ac477628f5aef
kin tirumala <kintirumala@gmail.com>
2025-09-13 13:23:08 -0400

    basic video creation + saving

---
+++ get_vid.py
@@ -0,0 +1,38 @@
+import cv2 as cv
+import numpy as np
+import os
+
+def create_dir_if_not_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def record_video():
+    cam = cv.VideoCapture(0)
+
+    output = cv.VideoWriter('./video/rant.avi', 
+                   cv.VideoWriter_fourcc(*'XVID'), 20.0, (640,480))
+
+    while cam.isOpened():
+        ret, frame = cam.read()
+        if ret:
+            output.write(frame)
+            cv.imshow('frame', cv.flip(frame, 1)) # mirror
+            if cv.waitKey(1) == ord('q'):
+                break
+
+    output.release()
+    cam.release()
+    cv.destroyAllWindows()
+
+def main():
+
+    # create a directory
+    create_dir_if_not_exists(f'./video')
+
+    # record the video
+    record_video()
+
+
+
+if __name__ == "__main__":
+    main()
